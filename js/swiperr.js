13f941d4d79efbbd77e8e855d1c2bf07bc17abf8
YASSIN1901 <yasynbdallhalwsaby@gmail.com>
2025-09-12 06:58:06 +0300

    dfdf

--- js/swiperr.js
+++ js/swiperr.js
@@ -16,20 +16,41 @@
 
 
     
-     /* swiper slide products */
+  /* swiper slide products */
 
-    var swiper = new Swiper(".slide_product", {
-  slidesPerView: 5,
-  spaceBetween: 20,
-  autoplay: {
-    delay: 2500,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-  loop: true
-});
+  var swiper = new Swiper(".slide_product", {
+    slidesPerView: 5,
+    spaceBetween:20,
+    autoplay:{
+        delay:2500,
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    loop:true,
+    breakpoints:{
+      1200:{
+        slidesPerView : 5,
+        spaceBetween: 20
+      },
+      1000:{
+        slidesPerView : 4,
+        spaceBetween: 20
+      },
+      700:{
+        slidesPerView: 3 , 
+        spaceBetween: 15 ,
+
+      },
+      0:{
+        slidesPerView : 2,
+        spaceBetween: 10
+      }
+    }
+    
+  });
+
 
 
     
